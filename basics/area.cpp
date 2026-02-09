bf162af651930eeb54e0e0951c8b24eede443f90
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-02-09 12:35:12 +0530

    .

---
+++ basics/area.cpp
@@ -0,0 +1,15 @@
+// Question1:In a program,input the side of a square.You have to output the area of the square.
+// Input:n(side)
+// Output:n*n(area)
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Input the side: ";
+    cin >> n;
+    cout << n*n<<endl;
+
+
+}
+
