40ad864be6d75a94b8dac16127329fb98b269703
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-02-09 13:10:22 +0530

    .

---
+++ basics/capacity.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    float a = 3.618419626461;
+    double b = 3.618419626461;
+
+    cout <<setprecision(12)<< a <<endl;
+    cout <<setprecision(12)<< b <<endl;
+}
