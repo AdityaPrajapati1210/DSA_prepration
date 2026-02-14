68c2f374faa123c32450425a256badc1ee1a2027
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-02-14 14:12:14 +0530

    .

--- loop/sumOfDigit.cpp
+++ loop/sumOfDigit.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
+    int n,sum=0;
     cout <<"Enter a number :";
     cin >> n;
     while(n >0){
         int rem = n%10;
-        
+        sum += rem;
+        n/=10;
     }
+    cout << sum << endl;
 }
