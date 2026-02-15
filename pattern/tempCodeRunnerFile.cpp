80dc44000e686dcf3d50730e1ea1b27560305508
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-02-15 15:46:36 +0530

    .

--- pattern/tempCodeRunnerFile.cpp
+++ pattern/tempCodeRunnerFile.cpp
@@ -17,17 +17,3 @@ int main(){
         }
         cout<<endl;
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i;j++){
-            cout << "* ";
-        }
-        for(int j=0;j<2*i+1;j++){
-            cout << "  ";
-        }
-        for(int j=0;j<n-i;j++){
-            cout << "* ";
-        }
-        cout<<endl;
-    }
-    
-}
