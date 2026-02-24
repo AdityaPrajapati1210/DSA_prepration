5fc6f75d182f4ad4f2d35b3459a022bab9fdc51a
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-02-24 23:39:14 +0530

    .

---
+++ pointers/address.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a  = 19;
+    cout << &a << endl;   //thar show the address where a is stored.
+    cout << &a+1 << endl;
+    cout << &a+2 << endl;
+    cout << &a+3 << endl;
+    cout << &a+4 << endl;
+
+    // this address is in hexadecimal form .
+}
