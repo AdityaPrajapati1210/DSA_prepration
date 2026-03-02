3f588e6e4105ff77926ec5a41bea10078b4a00cb
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-03-02 22:58:21 +0530

    .

---
+++ sorting Technic/insertion.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {5,4,2,56,2,56,8,7,6,5,};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=1;i<n;i++){
+       int curr = arr[i];
+       int prev = i-1;
+       while(prev >= 0 && arr[prev] > arr[prev +1]){
+            swap(arr[prev] , arr[prev +1]);
+            prev--;
+       }
+       curr = arr[prev+1];
+    }
+    // print array
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
