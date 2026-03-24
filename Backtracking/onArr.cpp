3897d467df5b0465006eee32215aa4c31485a055
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-03-24 20:27:54 +0530

    .

---
+++ Backtracking/onArr.cpp
@@ -0,0 +1,30 @@
+// Introduction
+
+#include<iostream>
+using namespace std;
+
+void printArr(int *arr,int n){
+    for (int  i= 0;i < n;i++){
+        cout << arr[i] <<" ";
+    }
+    cout << endl;
+}
+
+
+void changeArr(int arr[] , int n , int i){
+    if(i == n ){
+        printArr(arr , n);
+        return;
+    }
+
+    arr[i] = i+1;
+    changeArr(arr , n , i+1);
+    arr[i] -= 2;  //backtrack
+}
+
+int main(){
+    int n = 5;
+    int arr[n];
+    changeArr(arr , n , 0);
+    printArr(arr , n);
+}
