8b21ec1f6fd496834f1a62a65560214536a76e14
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-03-26 23:47:16 +0530

    .

--- Backtracking/gridWay.cpp
+++ Backtracking/gridWay.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
-int ways(int n, int m){
-    if(n == 0 && m == 0){
-        
+int ways(int r , int c,int n, int m){
+    if(r == n-1 && c == m-1){
+        return 1;
     }
+    if(r >= n || c >= m){
+        return 0;
+    }
+
+    return ways(r , c+1,n, m) + ways(r+1, c,n, m);
 }
 
 int main(){
-    int n = 4, m =4;
-    cout << ways(n,m) <<endl;
+    int n = 4, m = 4;
+    cout << ways(0,0,n, m) << endl;
 }
-
