c16336a67c2db82917aecdc7e6dcacea14553539
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-03-29 21:55:46 +0530

    .

--- Linked List(part-1)/l.cpp
+++ Linked List(part-1)/l.cpp
@@ -10,6 +10,13 @@ class Node{
         this->data = data;
         next = NULL;
     }
+    ~Node(){
+        cout<<"Node"<<data<<endl;
+        if(next != NULL){
+            delete next;
+            next = NULL;
+        }
+    }
 };
 
 class List{
@@ -41,7 +48,24 @@ class List{
     }
 
 
-    // INS
+    // Insert in the middle
+    void insertpos(int val,int pos){
+        Node*newnode  = new Node(val);
+        if(pos == 1){
+            newnode->next = head;
+            head  = newnode;
+            return;
+
+        }
+        Node*temp = head;
+        while(pos > 1){
+            temp = temp->next;
+            pos--;
+        }
+        newnode->next = temp->next;
+        temp->next = newnode;
+    }
+
 
     void print(){
         Node* temp = head;
@@ -50,6 +74,19 @@ class List{
             temp = temp->next;
         }
         cout << "NULL";
+    }
+
+
+    void popfront(){
+        
+    }
+
+    ~List(){
+        cout<<"list"<<endl;
+        if(head != NULL){
+            delete head;
+            head = NULL;
+        } 
     }
 };
 
@@ -60,6 +97,8 @@ int main(){
    List abc;
    abc.pushfront(10);
    abc.pushfront(20);
+   abc.pushback(20);
+   abc.insertpos(30,3);
    abc.pushback(20);
    abc.print();
 
