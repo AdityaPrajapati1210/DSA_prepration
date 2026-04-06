8e945fd40304b11bbc4504c64d83dd82ef9d392a
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-04-06 22:10:36 +0530

    .

---
+++ Greedy Algorithm/fractional_knalsack.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using  namespace std;
+
+int maximumAmount(vector<int>value,vector<int>weight,int w){
+    vector<float>ratio;
+    for(int i=0;i<value.size();i++){
+        ratio.push_back((float)value[i]/weight[i]);
+    }
+    
+    vector<pair<int,float>>ratioSort(value.size());
+
+    for(int i=0;i<value.size();i++){
+        ratioSort[i].first = i;
+        ratioSort[i].second = ratio[i];
+    }
+
+    sort(ratioSort.begin(),ratioSort.end(),[](auto &a ,auto &b){
+        return a.second > b.second;
+    });
+
+    int amount = 0;
+
+    for(int i=0;i<value.size();i++){
+        if(weight[ratioSort[i].first] <= w){
+            amount += value[ratioSort[i].first];
+            w = w - weight[ratioSort[i].first];
+        }else{
+            amount += w * ratioSort[i].second;
+            return amount;
+        }
+    }
+
+    return amount; 
+}
+
+int main(){
+    vector<int>value = {100 , 100, 120};
+    vector<int>weight = {20 , 10, 30};
+    int w = 50;
+    cout<<maximumAmount(value,weight,w)<<endl;
+}
