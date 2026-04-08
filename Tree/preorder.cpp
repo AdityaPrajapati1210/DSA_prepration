325265092769f600b4403e70dada540695102252
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-04-08 11:52:14 +0530

    .

---
+++ Tree/preorder.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+    Node*left;
+    Node*right;
+
+    Node(int data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* preorder(vector<int>vec){
+    static int idx = -1;
+    idx++;
+    if(idx<vec.size() && vec[idx] == -1){
+        return NULL;
+    }
+
+    Node*currnode = new Node(vec[idx]);
+    currnode->left = preorder(vec);
+    currnode->right = preorder(vec);
+    return currnode;
+}
+
+
+
+void print(Node*tree){
+    if(tree == NULL){
+        return;
+    }
+    cout<<tree->data<<" ";
+    print(tree->left);
+    print(tree->right);
+}
+
+int main(){
+    vector<int>vec = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    Node*tree = preorder(vec);
+    print(tree);
+
+}
