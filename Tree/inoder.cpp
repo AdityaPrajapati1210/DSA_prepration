56c2f0d99c754d76b7c28eb756c6b68d4ffe39a4
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-04-08 11:53:25 +0530

    .

---
+++ Tree/inoder.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+    Node*left;
+    Node*right;
+
+    Node(int data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* preorder(vector<int>vec){
+    static int idx = -1;
+    idx++;
+    if(idx<vec.size() && vec[idx] == -1){
+        return NULL;
+    }
+
+    Node*currnode = new Node(vec[idx]);
+    currnode->left = preorder(vec);
+    currnode->right = preorder(vec);
+    return currnode;
+}
+
+Node*postorder(vector<int>vec){
+    static int idx = -1;
+    idx++;
+    if(idx<vec.size() && vec[idx] == -1){
+        return NULL;
+    }
+
+    Node*currnode;
+    currnode->left = preorder(vec);
+    currnode->right = preorder(vec);
+    currnode = new Node(vec[idx]);
+    return currnode;
+}
+
+void printInorder(Node*tree){
+    if(tree == NULL){
+        return;
+    }
+    printInorder(tree->left);
+    cout<<tree->data<<" ";
+    printInorder(tree->right);
+}
+
+int main(){
+    vector<int>vec = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    Node*tree = preorder(vec);
+    printInorder(tree);
+
+}
