789b16615761690e0c05f6f24c632b059a2731aa
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-06-20 17:14:51 +0530

    using deque

--- Heap(part-2)/slidingWindowMax.cpp
+++ Heap(part-2)/slidingWindowMax.cpp
@@ -1,49 +1,89 @@
+// #include<iostream>
+// #include<queue>
+// #include<vector>
+// using namespace std;
+
+
+// struct compare{
+//     bool operator()(pair<int,int> &a, pair<int ,int> &b){
+//         return a.first < b.first;
+//     }
+// };
+
+
+// vector<int> maxSlide(vector<int>nums,int k){
+//     priority_queue<pair<int,int>,vector<pair<int,int>>,compare>pq;
+//     vector<int>ans;
+
+//     for(int i=0;i<k;i++){
+//         pq.push(make_pair(nums[i] ,i));
+//     }
+//     ans.push_back(pq.top().first);
+
+//     for(int i=k;i<nums.size();i++){
+//         while(pq.top().second <= i-k){
+//             pq.pop();
+//         }
+//         pq.push(make_pair(nums[i] ,i));
+//         ans.push_back(pq.top().first);
+//     }
+//     return ans;
+
+// }
+
+
+// int main(){
+//     vector<int>nums = { 1,3,-1,-3,5,2,5,3,1,5,8,-1};
+//     int k = 3;
+//     vector<int>ans = maxSlide(nums,k);
+    
+
+//     // print the vector
+
+//     int sum  =0;
+//     for(int i=0;i<ans.size();i++){
+//         cout<<ans[i]<<" ";
+//         sum += ans[i];
+//     }
+//     cout<<endl<<sum<<endl;
+// }
+
+
 #include<iostream>
-#include<queue>
 #include<vector>
+#include<queue>
 using namespace std;
 
 
-struct compare{
-    bool operator()(pair<int,int> &a, pair<int ,int> &b){
-        return a.first < b.first;
-    }
-};
-
-
-vector<int> maxSlide(vector<int>nums,int k){
-    priority_queue<pair<int,int>,vector<pair<int,int>>,compare>pq;
+vector<int>maxSlide(vector<int>nums , int k){
+    deque<int>dq;
     vector<int>ans;
+    for(int i=0;i<nums.size();i++){
 
-    for(int i=0;i<k;i++){
-        pq.push(make_pair(nums[i] ,i));
-    }
-    ans.push_back(pq.top().first);
-
-    for(int i=k;i<nums.size();i++){
-        while(pq.top().second <= i-k){
-            pq.pop();
+        if(!dq.empty() && dq.front() <= i-k){
+            dq.pop_front();
         }
-        pq.push(make_pair(nums[i] ,i));
-        ans.push_back(pq.top().first);
+
+        while(!dq.empty() && nums[dq.back()] < nums[i]){
+            dq.pop_back();
+        }
+
+        dq.push_back(i);
+
+        if(i >= k-1){
+            ans.push_back(nums[dq.front()]);
+        }
     }
     return ans;
-
 }
-
 
 int main(){
     vector<int>nums = { 1,3,-1,-3,5,2,5,3,1,5,8,-1};
-    int k = 3;
+    int k;
     vector<int>ans = maxSlide(nums,k);
-    
 
-    // print the vector
-
-    int sum  =0;
     for(int i=0;i<ans.size();i++){
-        cout<<ans[i]<<" ";
-        sum += ans[i];
+        cout << ans[i]<<" ";
     }
-    cout<<endl<<sum<<endl;
+
 }
