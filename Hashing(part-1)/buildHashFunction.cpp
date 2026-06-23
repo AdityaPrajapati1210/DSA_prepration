c66224cb4b1b923956da6f2dac5b112b4904d9f0
AdityaPrajapati1210 <aditya20051012@gmail.com>
2026-06-23 23:39:52 +0530

    .

--- Hashing(part-1)/buildHashFunction.cpp
+++ Hashing(part-1)/buildHashFunction.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<list>
 using namespace std;
+
+list<int>ll;
 
 class Node{
 public:
@@ -14,12 +17,29 @@ public:
         this->val = val;
         Node*next = NULL;
     }
+
+    ~Node(){
+        if(next != NULL){
+            delete next;
+        }
+    }
 };
 
 class HashFunc{
     int totsize;
     int currsize;
     Node**table;
+
+    int HashFunction(string key){
+        int n = key.length();
+
+        int idx= 0;
+        for(int i=0;i<n;i++){
+            idx += (key[i] * key[i])%totsize;
+        }
+        return idx;
+
+    }
 public:
 
     HashFunc(int size){
@@ -33,11 +53,50 @@ public:
         }
     }
 
-    void insert(string key, int val){
 
+    void rehash(){
+        Node**oldtable = table;
+        int oldsize = totsize;
+
+        table = new Node*[2*totsize];
+
+        for(int i=0;i<oldsize;i++){
+            table[i] = NULL;
+        }
+
+        for(int i=0;i<oldsize;i++){
+            Node*temp = oldtable[i];
+            while(temp != NULL){
+                insert(temp->key, temp->val);
+                temp = temp->next;
+            }
+
+            if(oldtable[i] != NULL){
+                delete oldtable[i];
+            }
+        }
+        delete [] oldtable;
     }
 
-    void eraise(string key){
+    void insert(string key, int val){
+        int idx = HashFunction(key);
+
+        Node* newNode = new Node(key ,val);
+        Node* head = table[idx];
+
+        newNode->next = head;
+        newNode = head;
+
+        currsize++;
+
+
+        int lamda = currsize/(double)totsize;
+        if(lamda > 1){
+            rehash();
+        }
+    }
+
+    void eraise(string key){ 
 
     }
 
@@ -51,5 +110,7 @@ int main(){
     obj.insert("Adiyta",20);
     obj.find("Aditya");
     obj.eraise("Adiyta");
+
+    
 
 }
