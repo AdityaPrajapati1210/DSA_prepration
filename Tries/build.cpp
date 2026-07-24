817888ce83e75db9db236d691a10be1805d156f1
AdityaPrajapati1210 <188994428+AdityaPrajapati1210@users.noreply.github.com>
2026-07-24 19:30:34 +0530

    .

---
+++ Tries/build.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Node{
+    public:
+    char ch;
+    Node * next;
+
+    Node(char ch){
+        this->ch = ch;
+    }
+
+}
+
+int main(){
+
+}
