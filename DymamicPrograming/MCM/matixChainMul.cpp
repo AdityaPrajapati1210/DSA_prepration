c83a75e78b8fb0cc2950f792c33370b0dbfce6fb
AdityaPrajapati1210 <188994428+AdityaPrajapati1210@users.noreply.github.com>
2026-09-03 14:27:31 +0530

    mcm using memoization

--- DymamicPrograming/MCM/matixChainMul.cpp
+++ DymamicPrograming/MCM/matixChainMul.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 
-int helper(vector<int>arr, int i, int j){
+int helper(vector<int>&arr, int i, int j,vector<vector<int>>&dp){
     if(i == j) return 0;
+
+    if(dp[i][j] != -1){
+        return dp[i][j];
+    }
     int ans= INT16_MAX;
 
     for(int k=i;k<j;k++){
-        int cost1 = helper(arr,i , k);
-        int cost2 = helper(arr,k+1,j);
+        int cost1 = helper(arr,i , k,dp);
+        int cost2 = helper(arr,k+1,j,dp);
         int currCost = cost1 + cost2 + arr[i-1]*arr[k]*arr[j];
         ans = min(ans,currCost);
     }
@@ -18,5 +22,7 @@ int helper(vector<int>arr, int i, int j){
 
 int main(){
     vector<int> arr = {1,2,3,4,3};
-    cout <<helper(arr,1,arr.size()-1);
+    int  n= arr.size();
+    vector<vector<int>>dp(n+1, vector<int>(n+1,-1));
+    cout <<helper(arr,1,n-1,dp);
 }
